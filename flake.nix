96013944cb277153a3afbb7f2f31d8902747742f
Justin van Son <justin.van.son@prodrive-technologies.com>
2023-10-13 11:49:00 +0200

    use separate homemanagerConfig and nixosSystem outputs

--- flake.nix
+++ flake.nix
@@ -1,33 +1,58 @@
 {
-  description = "Hyprland+waybar";
+  description = "My NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    hardware.url = "github:nixos/nixos-hardware";
+    impermanence.url = "github:nix-community/impermanence";
+    nix-colors.url = "github:misterio77/nix-colors";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprwm-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+
   };
 
   outputs = { self, nixpkgs, hyprland, home-manager, flake-utils
-    , pre-commit-hooks, ... }:
+    , pre-commit-hooks, ... }@inputs:
     let
+      inherit (self) outputs;
       inherit (flake-utils.lib) eachDefaultSystem;
-      system = "x86_64-linux";
-      buildSystem = { username, name }:
-        nixpkgs.lib.nixosSystem {
+      lib = nixpkgs.lib // home-manager.lib;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      pkgsFor = lib.genAttrs systems (system:
+        import nixpkgs {
           inherit system;
-          specialArgs = {
-            user = username;
-            inherit self;
-          };
+          config.allowUnfree = true;
+        });
+    in rec {
+      inherit lib;
+      homeManagerModules = import ./modules/home-manager;
+
+      nixosConfigurations = {
+        # Work laptop
+        lnxclnt2840 = lib.nixosSystem {
           modules = [
-            ./hosts/${name}/configuration.nix
+            ./hosts/dell-laptop-work/configuration.nix
             hyprland.nixosModules.default
             {
               programs.hyprland = {
@@ -35,26 +60,49 @@
                 xwayland.enable = true;
               };
             }
-            home-manager.nixosModules.home-manager
+          ];
+          specialArgs = {
+            user = "jusson";
+            inherit self inputs outputs;
+          };
+        };
+        # Main desktop
+        NixOs-justin = lib.nixosSystem {
+          modules = [
+            ./hosts/pc-i9_9900k-rtx3090/configuration.nix
+            hyprland.nixosModules.default
             {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { user = username; };
-                users.${username} = import ./home/${name};
+              programs.hyprland = {
+                enable = true;
+                xwayland.enable = true;
               };
             }
           ];
+          specialArgs = {
+            user = "justin";
+            inherit self inputs outputs;
+          };
         };
-    in rec {
-      nixosConfigurations = {
-        lnxclnt2840 = buildSystem {
-          name = "dell-laptop-work";
-          username = "jusson";
+      };
+
+      homeConfigurations = {
+        # Desktops
+        "jusson@lnxclnt2840" = lib.homeManagerConfiguration {
+          modules = [ ./home/dell-laptop-work ];
+          pkgs = pkgsFor.x86_64-linux;
+          extraSpecialArgs = {
+            user = "jusson";
+            users."jusson" = import home/dell-laptop-work;
+            inherit inputs outputs;
+          };
         };
-        NixOs-justin = buildSystem {
-          name = "pc-i9_9900k-rtx3090";
-          username = "justin";
+        "justin@NixOs-justin" = lib.homeManagerConfiguration {
+          modules = [ ./home/pc-i9_9900k-rtx3090 ];
+          pkgs = pkgsFor.x86_64-linux;
+          extraSpecialArgs = {
+            user = "justin";
+            inherit inputs outputs;
+          };
         };
       };
     } // eachDefaultSystem (system:
@@ -73,11 +121,10 @@
             deadnix.edit = true;
             deadnix.noLambdaArg = true;
           };
+
         };
         devShells.default = pkgs.mkShell {
           inherit (self.checks.${system}.pre-commit-check) shellHook;
         };
       });
 }
-#nixos-23.11
-
