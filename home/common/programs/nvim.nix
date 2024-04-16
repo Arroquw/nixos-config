de27413523852cf7b6d0b7ac0acc1ee010235312
Justin van Son <justin.van.son@prodrive-technologies.com>
2024-04-16 13:19:35 +0200

    Fix nvim

--- home/common/programs/nvim.nix
+++ home/common/programs/nvim.nix
@@ -1,21 +1,20 @@
-{ pkgs, ... }: {
+_: {
 
-  xdg.configFile."nvim".source = pkgs.stdenv.mkDerivation {
-    name = "NvChad";
-    src = pkgs.fetchFromGitHub {
-      owner = "NvChad";
-      repo = "NvChad";
-      rev = "e5f8a38ae3d6b3bedf68f29b0e96dad7a4ca2da5";
-      hash = "sha256-P5TRjg603/7kOVNFC8nXfyciNRLsIeFvKsoRCIwFP3I=";
-    };
-    installPhase = ''
-      mkdir -p $out
-      cp -r ./* $out/
-      cd $out/
-    '';
-  };
+  #xdg.configFile."nvim".source = pkgs.stdenv.mkDerivation {
+  #  name = "NvChad";
+  #  src = pkgs.fetchFromGitHub {
+  #    owner = "NvChad";
+  #    repo = "NvChad";
+  #    rev = "e5f8a38ae3d6b3bedf68f29b0e96dad7a4ca2da5";
+  #    hash = "sha256-P5TRjg603/7kOVNFC8nXfyciNRLsIeFvKsoRCIwFP3I=";
+  #  };
+  #  installPhase = ''
+  #    mkdir -p $out
+  #    cp -r ./* $out/
+  #    cd $out/
+  #  '';
+  #};
 
-  #cp -r ${./my_nvchad_config} $out/lua/custom
   programs.neovim = {
     enable = true;
     defaultEditor = true;
