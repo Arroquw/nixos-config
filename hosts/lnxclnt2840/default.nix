2c6e3a08c80ffb5f8cdf98414e81aa599cc0d1df
Justin van Son <justin.van.son@prodrive-technologies.com>
2024-04-16 13:20:11 +0200

    Add data/tools and data/projects mounts

--- hosts/lnxclnt2840/default.nix
+++ hosts/lnxclnt2840/default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ./hardware-configuration.nix
     ../common/global
@@ -35,18 +35,43 @@
     libusb1
   ];
 
-  fileSystems."/mnt/pd-common/copydrive" = {
-    device = "//prodrive.nl/copydrive/data";
-    fsType = "cifs";
-    options = [
-      "rw,noauto,users,file_mode=0664,dir_mode=0775,noserverino,nohandlecache,credentials=/etc/win-credentials"
-    ];
+  environment.etc."request-key.conf" = {
+    source = lib.mkForce (pkgs.writeText "request-key.conf" ''
+      create id_resolver * * /nix/store/4mgp5ybsvnhh11082gf5gix22dq8xwnv-nfs-utils-2.6.2/bin/nfsidmap -t 600 %k %d
+      create dns_resolver * * /run/current-system/sw/bin/key.dns_resolver %k
+    '');
   };
-  fileSystems."/mnt/pd-user/projects" = {
-    device = "//prodrive.nl/product";
-    fsType = "cifs";
-    options = [
-      "rw,noauto,users,file_mode=0664,dir_mode=0775,noserverino,nohandlecache,credentials=/etc/win-credentials"
-    ];
+
+  fileSystems = {
+    "/mnt/pd-common/copydrive" = {
+      device = "//prodrive.nl/copydrive/data";
+      fsType = "cifs";
+      options = [
+        "rw,noauto,users,file_mode=0664,dir_mode=0775,noserverino,nohandlecache,noperm,uid=1000,credentials=/etc/win-credentials"
+      ];
+    };
+    "/mnt/pd-user/projects" = {
+      device = "//prodrive.nl/product";
+      fsType = "cifs";
+      options = [
+        "rw,noauto,users,file_mode=0664,dir_mode=0775,noserverino,nohandlecache,noperm,uid=1000,credentials=/etc/win-credentials"
+      ];
+    };
+    "/data/tools" = {
+      device = "storage-nfs.prodrive.nl:/lnxtools";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "ro,nfsvers=3,proto=tcp,hard,intr,nolock,nosuid,nodev,noauto"
+      ];
+    };
+    "/data/projects" = {
+      device = "lnxdev02:/data/projects";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "nfsvers=3,proto=tcp,hard,intr,nolock,nosuid,nodev,noauto"
+      ];
+    };
   };
 }
