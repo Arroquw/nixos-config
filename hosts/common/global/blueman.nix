ea4bf9fc9741e944ecb5303758ab6f6d79f9bd2e
Justin van Son <justinvanson@outlook.com>
2024-11-21 19:44:55 +0100

    Add BT options

--- hosts/common/global/blueman.nix
+++ hosts/common/global/blueman.nix
@@ -1,4 +1,18 @@
-_: {
-  hardware = { bluetooth.enable = true; };
+{ pkgs, ... }: {
+  hardware = {
+    bluetooth = {
+      enable = true;
+      package = pkgs.bluez;
+      input.General.ClassicBondedOnly = false;
+    };
+  };
   services.blueman.enable = true;
+
+  services.udev.extraRules = ''
+    # PS4 DualShock controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0660", TAG+="uaccess"
+
+    # PS4 DualShock controller over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0660", TAG+="uaccess"
+  '';
 }
