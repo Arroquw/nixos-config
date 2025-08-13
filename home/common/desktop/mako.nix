381077d21955b447e6fe9630f3cac4c15b7c1fa6
Justin van Son <justinvanson@outlook.com>
2025-08-13 23:52:47 +0200

    Update mako config for new layout

--- home/common/desktop/mako.nix
+++ home/common/desktop/mako.nix
@@ -4,20 +4,22 @@
 
   services.mako = {
     enable = true;
-    font = "TeX Gyre Adventor 10";
-    iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
-    padding = "10,20";
-    anchor = "top-right";
-    width = 400;
-    height = 150;
-    borderSize = 0;
-    backgroundColor = "#1e1e2e";
-    textColor = "#cdd6f4";
-    borderColor = "#89b4fa";
-    progressColor = "over #313244";
+    settings = {
+      font = "TeX Gyre Adventor 10";
+      iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
+      padding = "10,20";
+      anchor = "top-right";
+      width = 400;
+      height = 150;
+      borderSize = 0;
+      backgroundColor = "#1e1e2e";
+      textColor = "#cdd6f4";
+      borderColor = "#89b4fa";
+      progressColor = "over #313244";
 
-    defaultTimeout = 12000;
-    layer = "overlay";
+      defaultTimeout = 12000;
+      layer = "overlay";
+    };
     extraConfig = ''
       on-notify=exec ${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play -i window-attention
       [app-name="prospect-mail"]
