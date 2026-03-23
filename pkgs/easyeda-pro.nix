e2f65f900015a4fb5c37e2f975fc0032f8927066
Justin van Son <justinvanson@outlook.com>
2026-03-23 08:55:54 +0100

    update easyEDA version

--- pkgs/easyeda-pro.nix
+++ pkgs/easyeda-pro.nix
@@ -3,12 +3,12 @@
 
 stdenv.mkDerivation rec {
   name = "easyeda-pro";
-  version = "2.2.37.3";
+  version = "2.2.45.4";
 
   src = fetchurl {
     url =
       "https://image.easyeda.com/files/easyeda-pro-linux-x64-${version}.zip";
-    sha256 = "sha256-X2g4yXzuWAz1vzG5C+6q62s8zxSj8r+EMkOJhQc6+nI=";
+    sha256 = "sha256-rLYVfPisNhVU2YiHVhCGQf5coWrPccYR2qbtVJENUAI=";
   };
 
   nativeBuildInputs = [ unzip autoPatchelfHook makeWrapper wrapGAppsHook3 ];
